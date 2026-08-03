a45c727b6ca26cd31fa5b646609205267e3978f1
hakeemalesh <hakeemalesh@gmail.com>
2026-08-04 00:42:06 +0100

    Sprint 21.5: align application version with release candidate

--- app/version.py
+++ app/version.py
@@ -7,6 +7,6 @@ This module stores the application's version details.
 APP_NAME = "HAPT"
 APP_FULL_NAME = "Hybrid AI Trading Platform"
 
-VERSION = "0.1.0"
+VERSION = "1.0.0-rc1"
 
 AUTHOR = "Hakeem Alesh"
